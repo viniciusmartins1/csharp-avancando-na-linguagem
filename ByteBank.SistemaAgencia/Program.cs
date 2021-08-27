a7c30846ea31fa33cd78aa4a5b26557ae96f92dc
Vinicius Martins <vm3090474@gmail.com>
2021-08-26 23:01:26 -0300

    Criando Estrutura do Projeto

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 324242);
+
+            Console.WriteLine(conta.Numero);
+
+            Console.WriteLine("");
+
+
+        }
+    }
+}
