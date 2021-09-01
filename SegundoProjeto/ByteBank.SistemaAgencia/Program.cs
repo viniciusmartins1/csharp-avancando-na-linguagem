3af799951dd8aed689487819d502828ce661cfff
Vinicius Martins <vm3090474@gmail.com>
2021-08-31 23:56:51 -0300

    Referencia de DLL

---
+++ SegundoProjeto/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
